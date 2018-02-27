b493cdfa7475acf19c20f3ef6e704de7c24dd4e8
Evan <wanlixi@users.noreply.github.com>
2018-02-27 17:34:25 +0800

    地址栏参数转为json
    地址栏参数转为json

--- utils.js
+++ utils.js
@@ -289,4 +289,17 @@ export const wxShare = (com_id) => {
   })
 }
 
-
+// 地址栏参数转为json
+export const parseQueryString = url => {
+ let reg_url = /^[^\?]+\?([\w\W]+)$/,
+  reg_para = /([^&=]+)=([\w\W]*?)(&|$|#)/g,
+  arr_url = reg_url.exec(url),
+  ret = {};
+ if (arr_url && arr_url[1]) {
+  let str_para = arr_url[1], result;
+  while ((result = reg_para.exec(str_para)) != null) {
+   ret[result[1]] = result[2];
+  }
+ }
+ return ret;
+}
